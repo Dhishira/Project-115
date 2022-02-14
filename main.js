e6aead9716defc80ee4db864950ccb477566b18d
Dhishira <84129757+Dhishira@users.noreply.github.com>
2022-02-14 19:29:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function setup()
     canvas = createCanvas(400 ,350);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(250, 300);
+    video.size(300, 250);
     video.hide();
 
     poseNet = ml5.poseNet(video,modelLoaded);
@@ -33,7 +33,7 @@ function gotPoses(results)
 
 function draw()
 {
-  image(video,0,0,300,350);
+  image(video,0,0,300,250);
 }
 
 function take_snapshot()
